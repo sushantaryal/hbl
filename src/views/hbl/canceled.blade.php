b394c26f9282c99b714c6ce212ac4d61a093034c
Bicky Raj Kayastha <bickyrajkayastha@gmail.com>
2022-11-28 00:50:10 +0100

    views updates

---
+++ src/views/hbl/canceled.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Payment Canceled'))
+@section('code', '404')
+@section('message', __("Sorry, the payment canceled please retry again."))
---
+++ src/views/hbl/failed.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Payment failed'))
+@section('code', '404')
+@section('message', __("Sorry, the payment failed please retry again."))
